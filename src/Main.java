482ff67ffda18ace7fd850f8ea5db807a4f1a7c5
User <git@github.com:BigFloopa-lab/HelloWorld.git>
2025-09-27 12:41:25 +0300

    Home work "for"

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("---1 задание---");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
+
+        System.out.println("---2 задание---");
+        for (int q = 10; q >= 1; q--) {
+            System.out.println(q);
+        }
+
+        System.out.println("---3 задание---");
+        for (int w = 2; w <= 17; w += 2) {
+            System.out.println(w);
+        }
+
+        System.out.println("---4 задание---");
+        for (int e = 10; e >= -10; e--) {
+            System.out.println(e);
+        }
+
+        System.out.println("---5 задание---");
+        for (int r = 1904; r < 2096; r += 4) {
+            System.out.println(r + " год является високосным");
+        }
+        System.out.println("---6 задание---");
+        for (int t = 7; t < 100; t += 7) {
+            System.out.println(t);
+        }
+        System.out.println("---7 задание---");
+        for (int y = 1; y < 515; y *= 2) {
+            System.out.println(y);
+        }
+        System.out.println("---8 задание---");
+        int sallary = 29000;
+        int months = 12;
+        int moneyInJar = 0;
+        for (int month = 1; month < months; month++) {
+            moneyInJar = moneyInJar + sallary;
+            System.out.println("Месяц " + month + " сумма накоплений равна " + moneyInJar);
+        }
+        System.out.println("---9 задание---");
+        int sallaryB = 29000;
+        int monthsB = 12;
+        int moneyInBank = 0;
+        for (int month = 1; month < monthsB; month++) {
+            moneyInBank = (moneyInBank + sallaryB) + (moneyInBank / 100 * 12);
+            System.out.println("Месяц " + month + " сумма накоплений равна " + moneyInBank);
+        }
+        System.out.println("---10 задание---");
+        int a = 2;
+        for (int c = 1; c <= 10; c++) {
+            System.out.println(a + "*" + c + "=" + (2 * c));
+        }
+
     }
+
+
 }
+
+
